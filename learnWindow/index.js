05c341372a1e34f823adc30ec9a670f4f98fcb45
BenJStoffels <beertje.stoffels@telenet.be>
2018-09-26 13:49:00 +0200

    finishing styling and adding vue to learnWindow

--- learnWindow/index.js
+++ learnWindow/index.js
@@ -1,12 +1,21 @@
 const electron = require('electron');
 
 const curwindow = electron.remote.getCurrentWindow();
-
 const woorden = curwindow.woorden;
 const correct = [];
-console.log(woorden);
 let currentIndex = Math.floor(Math.random() * woorden.length);
-let currentWoord = woorden[currentIndex];
+let currentWoord = curwindow.woorden[currentIndex];
+
+const app = new Vue({
+    el: '.container',
+    data: {
+        woorden,
+        correct,
+        currentWoord,
+        currentIndex
+    }
+})
+
 show();
 
 function handleForm() {
@@ -14,26 +23,23 @@ function handleForm() {
     const nom = form.querySelector('label').innerText;
     const { gen, vert, geslacht } = form;
     if (checkWord({ nom: nom, gen: gen.value, vert: vert.value, geslacht: geslacht.value })) {
-        const resp = document.querySelector('#response');
-        resp.classList.remove('hiding');
-        resp.classList.add('correct');
-        resp.querySelector('div').innerText = `${nom}, ${gen.value}: ${vert.value} (${geslacht.value})`;
-        correct.push(woorden.splice(currentIndex, 1));
+        app.correct.push(app.woorden.splice(app.currentIndex, 1)[0]);
     } else {
         const resp = document.querySelector('#response');
-        resp.classList.remove('hiding');
-        resp.classList.add('wrong');
-        resp.querySelector('div').innerHTML = `<p>${currentWoord.nom}, ${currentWoord.gen}: ${currentWoord.vert} (${currentWoord.geslacht})</p>`;
+        resp.className = 'wrong';
+        resp.innerHTML = `<p>${app.currentWoord.nom}, ${app.currentWoord.gen}: ${app.currentWoord.vert} (${app.currentWoord.geslacht})</p>`;
     }
-    setTimeout(updateWoord, 2500);
+    setTimeout(() => {
+        updateWoord();
+        show();
+    }, 2500);
 }
 
 function updateWoord() {
-    try {
-        currentIndex = Math.floor(Math.random() * woorden.length);
-        currentWoord = woorden[currentIndex];
-        show();
-    } catch (err) {
+    if (app.woorden.length != 0) {
+        app.currentIndex = Math.floor(Math.random() * app.woorden.length);
+        app.currentWoord = app.woorden[app.currentIndex];
+    } else {
         curwindow.close();
     }
 }
@@ -41,14 +47,12 @@ function updateWoord() {
 function show() {
     document.querySelector('#response').className = 'hiding';
     const form = document.querySelector('form');
-    form.className = 'woord';
-    const label = form.querySelector('label');
-    label.innerText = `${currentWoord.nom}, `;
     form.gen.value = '';
     form.vert.value = '';
     form.geslacht.value = 'm';
 }
 
 function checkWord(word) {
+    const currentWoord = app.currentWoord;
     return currentWoord.gen == word.gen && currentWoord.geslacht == word.geslacht && currentWoord.vert.find(v => v == word.vert);
 }
